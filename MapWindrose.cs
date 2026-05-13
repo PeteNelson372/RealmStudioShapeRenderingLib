03d335eb491acb9833554a0d9390aa55fb276bc3
PeteNelson372 <pete-nelson@msn.com>
2026-05-12 21:46:56 -0700

    Implemented windroses.

---
+++ MapWindrose.cs
@@ -0,0 +1,144 @@
+﻿/**************************************************************************************************************************
+* Copyright 2024, Peter R. Nelson
+*
+* This file is part of the RealmStudio application. The RealmStudio application is intended
+* for creating fantasy maps for gaming and world building.
+*
+* RealmStudio is free software: you can redistribute it and/or modify it under the terms
+* of the GNU General Public License as published by the Free Software Foundation,
+* either version 3 of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program.
+* The text of the GNU General Public License (GPL) is found in the LICENSE.txt file.
+* If the LICENSE.txt file is not present or the text of the GNU GPL is not present in the LICENSE.txt file,
+* see https://www.gnu.org/licenses/.
+*
+* For questions about the RealmStudio application or about licensing, please email
+* support@brookmonte.com
+*
+***************************************************************************************************************************/
+using SkiaSharp;
+
+namespace RealmStudioShapeRenderingLib
+{
+    public class MapWindrose : MapComponent2D
+    {
+        public Guid WindroseGuid { get; set; } = Guid.NewGuid();
+        public SKPoint Location { get; set; }
+        public SKColor WindroseColor { get; set; } = SKColor.Parse("#7F3D3728");
+        public int DirectionCount { get; set; } = 16;
+        public int LineWidth { get; set; } = 2;
+        public int InnerRadius { get; set; }
+        public int OuterRadius { get; set; } = 1000;
+        public int InnerCircles { get; set; }
+        public bool FadeOut { get; set; }
+
+        public SKPaint? _windrosePaint;
+
+        public override void Render(SKCanvas canvas, FontManager? fontManager = null)
+        {
+            _windrosePaint = new()
+            {
+                Style = SKPaintStyle.Stroke,
+                StrokeWidth = LineWidth,
+                Color = WindroseColor,
+                IsAntialias = true,
+            };
+
+            // draw circles, if any
+            if (InnerRadius > 0)
+            {
+                DrawWindroseLines(canvas, true);
+
+                switch (InnerCircles)
+                {
+                    case 1:
+                        canvas.DrawCircle(Location.X, Location.Y, InnerRadius, _windrosePaint);
+                        break;
+                    case 2:
+                        canvas.DrawCircle(Location.X, Location.Y, InnerRadius / 2, _windrosePaint);
+                        canvas.DrawCircle(Location.X, Location.Y, InnerRadius, _windrosePaint);
+                        break;
+                }
+            }
+            else
+            {
+                DrawWindroseLines(canvas, false);
+            }
+        }
+
+        private void DrawWindroseLines(SKCanvas canvas, bool fromCircle)
+        {
+            float circleStep = 360.0F / DirectionCount;
+
+            for (int i = 0; i < DirectionCount; i++)
+            {
+                SKPoint sp;
+
+                if (fromCircle)
+                {
+                    // get the starting point on the perimeter of the inner circle
+                    sp = Utilities.PointOnCircle(InnerRadius, i * circleStep, Location);
+                }
+                else
+                {
+                    // get the starting point at the cursor point
+                    sp = Location;
+                }
+
+                // get the ending point at the outer radius
+                SKPoint ep = Utilities.PointOnCircle(OuterRadius, i * circleStep, Location);
+
+                if (FadeOut)
+                {
+                    DrawFadedLine(canvas, sp, ep);
+                }
+                else
+                {
+                    canvas.DrawLine(sp, ep, _windrosePaint);
+                }
+            }
+        }
+
+        private void DrawFadedLine(SKCanvas canvas, SKPoint sp, SKPoint ep)
+        {
+            if (_windrosePaint != null)
+            {
+                SKPoint[] linePoints = Utilities.GetPoints(254, sp, ep);
+                int segmentOpacity = WindroseColor.Alpha;
+
+                SKPaint fadedLinePaint = _windrosePaint.Clone();
+
+                for (int j = 0; j < linePoints.Length - 1; j++)
+                {
+                    // draw line between points in linePoints, decreasing opacity
+                    fadedLinePaint.Color = new SKColor(WindroseColor.Red, WindroseColor.Green, WindroseColor.Blue, (byte)segmentOpacity); ;
+
+                    canvas.DrawLine(linePoints[j], linePoints[j + 1], fadedLinePaint);
+
+                    segmentOpacity--;
+                    segmentOpacity = Math.Max(segmentOpacity, 0);
+                }
+            }
+        }
+
+        public override bool HitTest(SKPoint worldPos)
+        {
+            return false;
+        }
+
+        public override IShapeState CaptureState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RestoreState(IShapeState state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Utilities.cs
+++ Utilities.cs
@@ -4,6 +4,40 @@ namespace RealmStudioShapeRenderingLib
 {
     public static class Utilities
     {
+        private const float PI_OVER_180 = (float)Math.PI / 180F;
+
+        public static SKPoint PointOnCircle(float radius, float angleInDegrees, SKPoint origin)
+        {
+            // Convert from degrees to radians via multiplication by PI/180
+            float angleInRadians = angleInDegrees * PI_OVER_180;
+
+            float x = (float)(radius * Math.Cos(angleInRadians)) + origin.X;
+            float y = (float)(radius * Math.Sin(angleInRadians)) + origin.Y;
+
+            return new SKPoint(x, y);
+        }
+
+        public static SKPoint[] GetPoints(uint quantity, SKPoint p1, SKPoint p2)
+        {
+            var points = new SKPoint[quantity];
+
+            double distance = SKPoint.Distance(p1, p2);
+
+            points[0] = p1;
+            points[quantity - 1] = p2;
+
+            double tdelta = distance / quantity;
+
+            for (int i = 1; i < quantity; i++)
+            {
+                double t = (i * tdelta) / distance;
+                points[i].X = (int)Math.Ceiling((1 - t) * p1.X + t * p2.X);
+                points[i].Y = (int)Math.Ceiling((1 - t) * p1.Y + t * p2.Y);
+            }
+
+            return points;
+        }
+
         public static SKPath BuildPath(IReadOnlyList<SKPoint> points)
         {
             var path = new SKPath();
